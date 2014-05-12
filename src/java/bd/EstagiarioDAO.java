33be14d272dad86b2cf7e67356aeb02ce80986dc
RenanHufSilveira <rh.silveira93@gmail.com>
2014-05-12 20:17:41 -0300

    Documentos Iniciais
    Foram terminados as classes DAO de contatoestagiario e endereco estágiario, foram praticamente terminados os layouts da pagina de cadastro de estagiario e editar estagiario

--- src/java/bd/EstagiarioDAO.java
+++ src/java/bd/EstagiarioDAO.java
@@ -28,70 +28,31 @@ public class EstagiarioDAO {
     }
 
     public void inserir(Estagiario es) {
-        String sql = "INSERT INTO curso VALUES(default, ?, ?, ?);";
+        String sql = "INSERT INTO estagiario VALUES(default, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
         try {
             pstm = con.conectar().prepareStatement(sql);
-            pstm.setString(1, es.getNome());
+            pstm.setInt(1, es.getMatricula());
+            pstm.setString(2, es.getNome());
+            pstm.setString(3, es.getRG());
+            pstm.setString(4, es.getCPF());
+            pstm.setString(5, es.getDtNascimento());
+            pstm.setString(6, es.getPai());
+            pstm.setString(7, es.getMae());
+            pstm.setString(10, es.getDisponibilidadeHorarioEstagio());
+            pstm.setString(11, es.getEmpresaTrabalha());
+            pstm.setString(12, es.getHorarioTrabalha());
+            
             pstm.executeQuery();
             pstm.close();
             con.desconectar();
         } catch (SQLException ex) {
 
         }
-    }
-
-    public void alterarNome(Estagiario es) {
-        String sql = "UPDATE curso SET nome=? where id=?";
-        try {
-            pstm = con.conectar().prepareStatement(sql);
-            pstm.setString(1, es.getNome());
-            pstm.setInt(2, es.getId());
-            pstm.executeQuery();
-            pstm.close();
-            con.desconectar();
-        } catch (SQLException ex) {
-
-        }
-    }
-
-    public void alterarTipo(Estagiario es) {
-        String sql = "UPDATE curso SET tipo=? where id=?";
-        try {
-            pstm = con.conectar().prepareStatement(sql);
-            pstm.setInt(2, es.getId());
-            pstm.executeQuery();
-            pstm.close();
-            con.desconectar();
-        } catch (SQLException ex) {
-
-        }
-    }
-
-    public ArrayList<Estagiario> consultar(String nome) {
-        
-        String sql = "SELECT * FROM curso WHERE nome like ? and status = 1;";
-        ArrayList<Estagiario> listCurso = new ArrayList();
-        try {
-            pstm = this.con.conectar().prepareStatement(sql);
-            pstm.setString(1, "%"+nome+"%");
-            rs = pstm.executeQuery();
-
-            while (rs.next()) {    //move o curso de registros
-                Estagiario es = new Estagiario();
-                es.setId(rs.getInt("id"));
-                listCurso.add(es);
-            }
-            pstm.close();
-            con.desconectar();
-        } catch (SQLException ex) {
-            Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return listCurso;
     }
 
     public ArrayList<Estagiario> listar() {
         ArrayList<Estagiario> lista = new ArrayList();
-        String sql = "SELECT * FROM curso where status = 1";
+        String sql = "SELECT * FROM estagiario ";
 
         try {
             pstm = this.con.conectar().prepareStatement(sql);
@@ -100,6 +61,16 @@ public class EstagiarioDAO {
             while (rs.next()) {    //move o curso de registros
                 Estagiario es = new Estagiario();
                 es.setId(rs.getInt("id"));
+                es.setMatricula(rs.getInt("matricula"));
+                es.setNome(rs.getString("nome"));
+                es.setRG(rs.getString("rg"));
+                es.setCPF(rs.getString("cpf"));
+                es.setDtNascimento("dtnascimento");
+                es.setPai("nomepai");
+                es.setMae("nomemae");
+                es.setDisponibilidadeHorarioEstagio("disponibilidadehorarioestagio");
+                es.setEmpresaTrabalha("empresatrabalha");
+                es.setHorarioTrabalha("horariotrabalha");                
                 lista.add(es);
                 
             }
@@ -109,51 +80,5 @@ public class EstagiarioDAO {
             Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return lista;
-    }
-
-    public void alterarCurso(Estagiario es) {
-        String sql = "UPDATE curso SET nome=? ,turno=? ,tipo=? where id=?";
-        try {
-            pstm = con.conectar().prepareStatement(sql);
-            pstm.setInt(4, es.getId());
-            pstm.executeQuery();
-            pstm.close();
-            con.desconectar();
-        } catch (SQLException ex) {
-
-        }
-    }
-
-    public void excluir(Estagiario es) {
-        String sql = "UPDATE curso SET status = ? where id = ?";
-        try {
-            pstm = con.conectar().prepareStatement(sql);
-            pstm.setInt(1, 0);
-            pstm.setInt(2, es.getId());
-            pstm.execute();
-
-        } catch (SQLException ex) {
- 
-        }
-    }
-    
-    public Estagiario consultarId(int id) {
-        Estagiario es = new Estagiario();
-        String sql = "SELECT * FROM curso WHERE id = ?";
-
-        try {
-            pstm = this.con.conectar().prepareStatement(sql);
-            pstm.setInt(1, id);
-            rs = pstm.executeQuery();
-
-            while (rs.next()) {    //move o curso de registros
-                es.setId(rs.getInt("id"));
-            }
-            pstm.close();
-            con.desconectar();
-        } catch (SQLException ex) {
-            Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return es;
     }
 }
